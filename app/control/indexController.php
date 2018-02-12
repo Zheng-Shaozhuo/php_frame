1e7c8b8e8656910cb8a064f65df990ae7703ebab
Shaozhuo <Zheng-Shaozhuo@outlook.com>
2018-02-12 15:00:57 +0800

    用户登录成功控制器类, 仅展示页面逻辑

--- app/control/indexController.php
+++ app/control/indexController.php
@@ -7,33 +7,22 @@
  */
 namespace app\control;
 
-class indexController extends \core\frame
+class IndexController extends BaseController
 {
     public function index()
     {
-        var_dump(__DIR__);
-        echo 'index';
+        $this->display('blist');
     }
 
-    public function admin()
-    {
-        var_dump(__DIR__);
-        echo 'admin';
-
-        var_dump($_GET);
+    public function addblog() {
+        $this->display('addblog');
     }
 
-    public function model()
-    {
-        $m = new \core\lib\model();
-        $o = $m->query("select * from user");
-        var_dump($o->fetchAll());
+    public function classify() {
+        $this->display('classify');
     }
 
-    public function view()
-    {
-        $data = 'Hello ZhengShaozhuo';
-        $this->assign('data', $data);
-        $this->display('index/index');
+    public function tags() {
+        $this->display('tags');
     }
 }
