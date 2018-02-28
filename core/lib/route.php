dd188e002cb9fea1dc77fe687113d3a4a0b715e6
Shaozhuo <Zheng-Shaozhuo@outlook.com>
2018-02-28 17:08:56 +0800

    添加自定义路由解析部分 Route, 需配置对应route.config.php文件

--- core/lib/route.php
+++ core/lib/route.php
@@ -7,6 +7,8 @@
  */
 namespace core\lib;
 
+use core\common\Config;
+
 class route
 {
     public $control;
@@ -51,9 +53,66 @@ class route
                     }
                 }
             }
+        } elseif ('/index.php' . $_SERVER['REQUEST_URI'] == $_SERVER['PHP_SELF']) {
+            $routes = Config::RC('route', true);
+            $target = $_SERVER['REQUEST_URI'];
+            $res = $this->getRewriteParam($routes, $target);
+            if ($res['state']) {
+                $this->control = $res['control'];
+                $this->action = $res['action'];
+            }
         } else {
             $this->control = 'index';
             $this->action = 'index';
         }
+    }
+
+    private function getRewriteParam($routes, $target) {
+        $state = true;
+        $control = null;
+        $action = null;
+        if (empty($routes) || empty($target)) {
+            return array('state' => $state, 'control' => $control, 'action' => $action);
+        }
+
+        $target = String::getRewriteValue($target);
+        foreach ($routes as $k => $v) {
+            if ($target == strtolower($k)) {
+                foreach ($v as $tk => $tv) {
+                    if (0 == $tk) {
+                        $params = explode('/', $tv);
+                        if (2 == count($params)) {
+                            $control = $params[0];
+                            $action = $params[1];
+                        } else {
+                            $state = false;
+                        }
+                    } else {
+                        if (1 == count($tv)) {
+                            $key = array_keys($tv)[0];
+                            $value = array_values($tv)[0];
+                            switch (strtolower($key)) {
+                                case 'method':
+                                    if ($_SERVER['REQUEST_METHOD'] != strtoupper($value)) {
+                                        $state = false;
+                                    }
+                                    break;
+                                default:
+                                    break;
+                            }
+                        }
+                    }
+
+                    if (!$state) {
+                        $control = null;
+                        $action = null;
+                        break;
+                    }
+                }
+            } else {
+                continue;
+            }
+        }
+        return array('state' => $state, 'control' => $control, 'action' => $action);
     }
 }
